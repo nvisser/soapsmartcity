8fba788d33b159e92a3232ec539f4466dcd6f78a
nvisser <niek@bcome.nl>
2017-03-11 15:10:56 +0100

    Add more tests

--- src/test/java/inwoners/WijkSoapBindingImplTest.java
+++ src/test/java/inwoners/WijkSoapBindingImplTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 /**
  * Created by niek on 3/11/17.
@@ -24,6 +25,16 @@ public class WijkSoapBindingImplTest {
 
     @Test
     public void calcInwonersVoorWijkNormal() throws Exception {
+        assertEquals(wijk.calcInwonersVoorWijk("test", 2015), 6450);
+        assertEquals(wijk.calcInwonersVoorWijk("Nieuwegein Zuid", 2015), 6458);
+        assertEquals(wijk.calcInwonersVoorWijk("Amsterdam Zuid", 2015), 6457);
+        assertEquals(wijk.calcInwonersVoorWijk("Amsterdam Bijlmer Arena", 2015), 6464);
+
+        assertEquals(wijk.calcInwonersVoorWijk("test", 2016), 6454);
+        assertEquals(wijk.calcInwonersVoorWijk("Nieuwegein Zuid", 2016), 6461);
+        assertEquals(wijk.calcInwonersVoorWijk("Amsterdam Zuid", 2016), 6461);
+        assertEquals(wijk.calcInwonersVoorWijk("Amsterdam Bijlmer Arena", 2016), 6467);
+
         assertEquals(wijk.calcInwonersVoorWijk("test", 2017), 6457);
         assertEquals(wijk.calcInwonersVoorWijk("Nieuwegein Zuid", 2017), 6464);
         assertEquals(wijk.calcInwonersVoorWijk("Amsterdam Zuid", 2017), 6464);
@@ -40,10 +51,26 @@ public class WijkSoapBindingImplTest {
 
     @Test
     public void calcInwonersVoorWijkYearZero() throws Exception {
-        assertEquals(wijk.calcInwonersVoorWijk("x", 0), 0);
-        assertEquals(wijk.calcInwonersVoorWijk("xx", 0), 1);
-        assertEquals(wijk.calcInwonersVoorWijk("xxx", 0), 2);
-        assertEquals(wijk.calcInwonersVoorWijk("xxxx", 0), 2);
+        assertEquals(wijk.calcInwonersVoorWijk("test", 0), 2);
+        assertEquals(wijk.calcInwonersVoorWijk("Nieuwegein Zuid", 0), 10);
+        assertEquals(wijk.calcInwonersVoorWijk("Amsterdam Zuid", 0), 9);
+        assertEquals(wijk.calcInwonersVoorWijk("Amsterdam Bijlmer Arena", 0), 16);
+    }
+
+    @Test
+    public void calcInwonersVoorWijkYearNegative() throws Exception {
+        assertEquals(wijk.calcInwonersVoorWijk("test", -1), 0);
+        assertEquals(wijk.calcInwonersVoorWijk("Nieuwegein Zuid", -1), 7);
+        assertEquals(wijk.calcInwonersVoorWijk("Amsterdam Zuid", -1), 6);
+        assertEquals(wijk.calcInwonersVoorWijk("Amsterdam Bijlmer Arena", -1), 12);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void calcInwonersVoorWijkYearNull() throws Exception {
+        wijk.calcInwonersVoorWijk(null, 2017);
+        wijk.calcInwonersVoorWijk(null, 2017);
+        wijk.calcInwonersVoorWijk(null, 2017);
+        wijk.calcInwonersVoorWijk(null, 2017);
     }
 
 }
